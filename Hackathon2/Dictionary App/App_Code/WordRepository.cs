2d10f61fe7b904088cad85bad7ba29df467e577a
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-08-26 12:22:22 +0530

    Create WordRepository.cs

---
+++ Hackathon2/Dictionary App/App_Code/WordRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWordsApp.App_Code
+{
+    public class WordRepository
+    {
+        public static Dictionary<string, string> Words = new Dictionary<string, string>()
+        {
+            { "fun", "" },
+            {"sequal", "" },
+            {"lambdas", "" },
+            {"expression", "" },
+            {"tree", "" },
+            {"query", "" },
+            {"language", "" },
+            
+        };
+    }
+}
