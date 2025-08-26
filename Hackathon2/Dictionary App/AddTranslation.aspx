aeda6085b85045cf208af0967d41f3c18d9f8ee4
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-08-26 12:21:09 +0530

    files committed

---
+++ Hackathon2/Dictionary App/AddTranslation.aspx
@@ -0,0 +1,25 @@
+﻿﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddTranslation.aspx.cs" Inherits="SearchWord.AddTranslation" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <h2>Add Translation</h2>
+        <div>        
+            <asp:Label ID="lblWord" runat="server" />
+            <asp:TextBox ID="txtTranslation" runat="server" />
+            <asp:Button ID="btnAdd" runat="server" Text="Add to My Words" OnClick="btnAdd_Click" />
+            <br /><br />
+            <asp:GridView ID="Words" runat="server" AutoGenerateColumns="false" >
+                <Columns>
+                    <asp:BoundField DataField="Word" HeaderText="Word" />
+                    <asp:BoundField DataField="Translation" HeaderText="Translation" />
+                 </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Hackathon2/Dictionary App/WordSearch.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SearchWord
+{
+    public partial class WordSearch : System.Web.UI.Page
+    {
+
+        public static Dictionary<string, string> wordList = new Dictionary<string, string>()
+        {
+            { "fun", "" },
+            { "hello", "" },
+            { "world", "" }
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            var word = txtSearchWord.Text.Trim().ToLower();
+            if (wordList.ContainsKey(word))
+            {
+                Response.Redirect($"AddTranslation.aspx?Word={word}");
+            }
+            else
+            {
+                lblMessage.Text = $"{word} word is not present in the application. Please try another word.";
+            }
+        }
+    }
+}
