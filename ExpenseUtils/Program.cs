54283572e37ff02bdc78c6d235b815cb816002c1
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:37:18 +0530

    Create Program.cs

---
+++ ExpenseUtils/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using AssignmentProject.UI;
+
+namespace AssignmentProject
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UIHandler.DisplayMenu();
+        }
+    }
+}
