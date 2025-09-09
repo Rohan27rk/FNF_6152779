4fe040c265b31711c1b8f08f53aa631a5e32292a
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 09:34:47 +0530

    Add files via upload

---
+++ WEB_PROGRAMMING/JavaScript/Ex01Basic.js
@@ -0,0 +1,8 @@
+alert('Test 123');
+let result = prompt('Enter a number');
+let answer = confirm('Are you sure?');
+if (answer != null) {
+    alert('Correct!');
+} else {
+    alert('Incorrect!');
+}
