d2b7c7e2bfdaefbb65447909524e257897feee73
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:27:17 +0530

    Create Employee.cs

---
+++ DotnetCoreApps/SampleMvcApp/Data/Employee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace SampleMvcApp.Data;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string EmpName { get; set; } = null!;
+
+    public string EmpAddress { get; set; } = null!;
+
+    public decimal EmpSalary { get; set; }
+
+    public int? DeptId { get; set; }
+}
