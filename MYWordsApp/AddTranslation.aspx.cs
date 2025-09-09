224e1b24d5cf29af2d655ef195ad0b0651980f11
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:03:38 +0530

    Add files via upload

---
+++ MYWordsApp/AddTranslation.aspx.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SearchWord
+{
+    public partial class AddTranslation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string word = Request.QueryString["word"];
+            lblWord.Text = $"Word: {word}";
+            if (!IsPostBack)
+            {
+                var wordList = WordSearch.wordList;
+
+                var displayList = new List<object>();
+                foreach (var entry in wordList)
+                {
+                    displayList.Add(new { Word = entry.Key, Translation = entry.Value });
+                }
+
+                Words.DataSource = displayList;
+                Words.DataBind();
+            }
+        }
+
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string word = Request.QueryString["Word"];
+            string translation = txtTranslation.Text.Trim();
+
+            WordSearch.wordList[word] = translation;
+
+            var wordList = WordSearch.wordList;
+
+            var displayList = new List<object>();
+            foreach (var str in wordList)
+            {
+                displayList.Add(new
+                {
+                    Word = str.Key,
+                    Translation = str.Value
+                });
+            }
+            Words.DataSource = displayList;
+            Words.DataBind();
+        }
+    }
+}
---
+++ MYWordsApp/WordSearch.aspx
@@ -0,0 +1,23 @@
+﻿﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WordSearch.aspx.cs" Inherits="SearchWord.WordSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Search Word</h2>
+        <div>    
+        <p>
+            English Word: <asp:TextBox ID="txtSearchWord" runat="server" />
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        </p>
+        
+        <asp:Label ID="lblMessage" runat="server" ForeColor="Red" />
+
+        </div>
+    </form>
+</body>
+</html>
