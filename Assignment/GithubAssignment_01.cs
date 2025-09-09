7b7430b8c2fa3a27ffe56998eedf974577854821
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:01:10 +0530

    Add files via upload

---
+++ Assignment/GithubAssignment_01.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Assignments
+{
+    class GithubAssignment_01
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            CountWordFrequencyDesc(str);
+        }
+
+        public static void CountWordFrequencyDesc(string? str)
+        {
+            List<string> words = new List<string>();
+            string Word = "";
+            foreach (char ch in str)
+            {
+                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                {
+                    Word += char.ToLower(ch);
+                }
+                else if (Word.Length > 0)
+                {
+                    words.Add(Word);
+                    Word = "";
+                }
+            }
+            if (Word.Length > 0)
+            {
+                words.Add(Word);
+            }
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                else
+                {
+                    dict[word] = 1;
+                }
+            }
+            //foreach (var item in Count)
+            //{
+            //    Console.WriteLine($"{item.Value} {item.Key}");
+            //}
+            var sorted = dict
+                .OrderByDescending(x => x.Value)
+                .OrderByDescending(x => x.Key);
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine($"{item.Value} {item.Key}");
+            }
+
+            //Console.WriteLine(list);
+        }
+    }
+}
+
+
