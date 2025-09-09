00d957f0fc4dc75e3ecf9f3d3c98769a7cfb808c
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:08:09 +0530

    Add files via upload

---
+++ DotnetCoreApps/Assignment/DbFirstApproach.cs
@@ -0,0 +1,71 @@
+﻿using Assignment.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class DbFirstApproach
+    {
+        static void Main(string[] args)
+        {
+            //InsertCustomer();
+            DeleteExample();
+        }
+
+        private static void DeleteExample()
+        {
+
+            try
+            {
+                var context = new FnftrainingContext();
+                Console.WriteLine("Enter Customer ID to delete the record");
+                int id = int.Parse(Console.ReadLine());
+                var cst = context.Customers.FirstOrDefault(c => c.CstId == id);
+
+                if(cst != null)
+                {
+                    context.Customers.Remove(cst);
+                    context.SaveChanges();
+                    Console.WriteLine("Customer deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Record not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Enter the valid ID ");
+            }
+
+
+
+        }
+
+
+        private static void InsertCustomer()
+        {
+            try
+            {
+                var context = new FnftrainingContext();
+                context.Customers.Add(new Customer
+                {
+                    CstName = "Rohan",
+                    CstAddress = "Bangalore",
+                    BillDate = DateTime.Now,
+                    BillAmount = 2563
+                });
+                context.SaveChanges();
+                Console.WriteLine("Customer Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DotnetCoreApps/Assignment/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Assignment
+{
+    using Assignment.Data;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            insertStudent();
+        }
+
+        private static void insertStudent()
+        {
+             try
+            {
+                var DbContext = new StudentsContext();
+                var NewStudent = new Students
+                {
+                    Name = "Rohan",
+                    Address = "Nanded",
+                    Standard = 10
+                };
+                DbContext.MyStudents.Add(NewStudent);
+                DbContext.SaveChanges();
+                Console.WriteLine("Data Added successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void updateExample()
+        {
+            try
+            {
+                var dbContext = new StudentsContext();
+                var rec = dbContext.MyStudents.Find(1);
+                if (rec != null)
+                {
+                    rec.Name = "The Great Wall";
+                    rec.Address = "John Cena";
+                    rec.Standard = 12;
+                }
+
+
+                else
+                {
+                    Console.WriteLine("No record found on this id");
+                }
+                dbContext.SaveChanges();
+                Console.WriteLine("Record updated Successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void getAllExample()
+        {
+            try
+            {
+                var context = new StudentsContext();
+                var records = context.MyStudents.ToList();
+                foreach (var record in records)
+                {
+                    Console.WriteLine(record.Name.ToUpper());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException?.Message);
+            }
+        }
+
+        private static void deleteExample()
+        {
+            try
+            {
+                var context = new StudentsContext();
+                var rec = context.MyStudents.Find(1);
+                if (rec == null)
+                {
+                    Console.WriteLine("No Record found to delete");
+                    return;
+                }
+                context.MyStudents.Remove(rec);
+                context.SaveChanges();
+                Console.WriteLine("Student deleted Successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException?.Message);
+            }
+        }
+    }
+
+}
+//    Scaffold-DBContext "Data Source=FNFIDVPRE22648;Initial Catalog=FNFTraining;Integrated Security=True;Encrypt=False;Trust Server Certificate=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data -Tables "Employee" 
