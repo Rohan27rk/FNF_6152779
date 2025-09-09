11e8ed191370ff75d83e47cf089ed4b896f3479f
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:28:47 +0530

    Add files via upload

---
+++ DotnetCoreApps/SampleMvcApp/Models/DataComponents.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleMvcApp.Models
+{
+    [Table("CstTable")]
+    public class Customer
+    {
+        [Key]
+        public int CstId { get; set; }
+
+        [Required (ErrorMessage ="Customer name is mandatory")]
+        public string CstName { get; set; }
+        [Required (ErrorMessage ="Customer address is mandatory")]
+        public string CstAddress { get; set; }
+        [Required (ErrorMessage ="Bill amount is mandatory")]
+        public double BillAmount { get; set; }
+    }
+
+    public class CstDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.UseSqlServer("Data Source=FNFIDVPRE22648;Initial Catalog=FNFTraining;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+
+            var connectionString = Program.Configuration["ConnectionStrings:MyCon"];
+            optionsBuilder.UseSqlServer(connectionString);
+
+
+        }
+
+    }
+}
---
+++ DotnetCoreApps/SampleMvcApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace SampleMvcApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+
+        public int EmpSalary { get; set; }
+        public int DeptId { get; set; }
+    
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {EmpName}, Address: {EmpAddress}, Salary: {EmpSalary}, Dept ID: {DeptId}";
+        }
+
+    }
+}
