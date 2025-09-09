d06910d0fd448cdd5a47f40fe13c666ce85a14e7
Rohan27rk <138243287+Rohan27rk@users.noreply.github.com>
2025-09-09 10:22:19 +0530

    Add files via upload

---
+++ DotnetCoreApps/SampleDotnetCoreApp/Data/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDotnetCoreApp.Data;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string EmpName { get; set; } = null!;
+
+    public string EmpAddress { get; set; } = null!;
+
+    public decimal EmpSalary { get; set; }
+
+    public int? DeptId { get; set; }
+
+    public virtual DeptTable? Dept { get; set; }
+}
